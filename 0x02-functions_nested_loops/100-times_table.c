b4c414af3de3bc8c029e5a03f0ff392c7d2735db
root <obafemiojo97@gmail.com>
2022-03-15 07:48:53 -0700

    last adv task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,5 @@
-#include "stdio.h"
 #include "main.h"
+
 /**
  *print_times_table - Print the `n` times table, starting with 0.
  *Description: If `n` is greater than 15 or less than 0, print nothing.
