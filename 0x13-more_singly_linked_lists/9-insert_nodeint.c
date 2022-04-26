f181083092af1a131fcb8c30ab1d8444af790019
obfemiojo <obafemiojo97@gmail.com>
2022-04-26 10:05:56 -0700

    9th commit

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * insert_nodeint_at_index - a function ...
+ * @head: the list
+ * @n: the chaine
+ * @idx: the number
+ *
+ * Return: 1 or 0
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *ptr = *head, *save;
+	unsigned int cpt = 0;
+
+	new =  malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (ptr == NULL || idx == 0)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	while (cpt != idx - 1 && ptr->next != NULL)
+	{
+		ptr = ptr->next;
+		cpt++;
+	}
+
+	if (ptr->next == NULL)
+		ptr->next = new;
+	else
+	{
+		save = ptr->next;
+		ptr->next = new;
+		new->next = save;
+	}
+	return (new);
+}
